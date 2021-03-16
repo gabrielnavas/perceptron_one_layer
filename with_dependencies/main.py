a2eecb56d716b59e82890bd722099fa4b7d9bea4
Navas <gabrielnavas@email.com>
2021-03-16 14:26:54 -0300

    refactory perceptron one layer with numpy

--- with_dependencies/main.py
+++ with_dependencies/main.py
@@ -1,27 +1,13 @@
 import numpy as np
 
-inputs = np.array([1,7,5])
-weights = np.array([0.8, 0.1, 0])
-
-def stepFunction(x: float) -> int:
-  if(x > 0.00):
-    return 1
-  return 0
-
-def sumInputWithWeight(inputs: [], weights: []) -> float:
-  """
-    dot product, use numpy 
-    to great performance 
-    sum = inputs[0] x weights[0] +
-    inputs[1] x weights[1] +
-    inputs[2] x weights[2] +
-  """ 
-  sum = inputs.dot(weights)
-  return sum
+stepFunction = lambda sum: 1 if sum > 0.00 else 0
 
 def main():
-  weightsSum = sumInputWithWeight(inputs, weights)
+  inputs = np.array([1, 7, 5])
+  weights = np.array([0.8, 0.1, 0])
+
+  weightsSum = inputs.dot(weights)
   oneOrTwo = stepFunction(weightsSum)
-  print(oneOrTwo) # 1
+  print('functions result: ', oneOrTwo) # 1
 
 main()
